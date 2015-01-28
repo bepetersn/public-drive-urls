698b0786c1457a31bd373bb7f5cfa3bf5d8f6b31
Brian Peterson <bepetersn@gmail.com>
2015-01-28 14:53:36 -0600

    update stuff for pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
     name="public-drive-urls",
-    version='0.0.1',
+    version='0.0.2',
     author="Brian Peterson",
     author_email="bepetersn@gmail.com",
     description="Find Google Drive download URLs from a file's sharing URL",
     license="MIT",
-    url="https://github.com/wilbertom/confyg",
+    url='https://github.com/bepetersn/public-drive-urls/',
     py_modules=['public_drive_urls'],
     classifiers=[
     ],
