4e9451ab19e12fd0ee904a59109404e938125863
Brian Peterson <bepetersn@gmail.com>
2017-02-21 19:37:02 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="public-drive-urls",
-    version='0.2.1',
+    version='0.3.0',
     author="Brian Peterson",
     author_email="bepetersn@gmail.com",
     description="Find Google Drive download URLs from a file's sharing URL",
